55a41b0b6999f89a1dec310c2a807d248ce01484
Ilya Zavershinskyi <83955333+StayOnSofa@users.noreply.github.com>
2024-02-18 20:55:29 +0600

    Update readme

--- Assets/Plugins/PoolingSlider/Example/ExampleSetup.cs
+++ Assets/Plugins/PoolingSlider/Example/ExampleSetup.cs
@@ -6,6 +6,8 @@ namespace PoolingSlider.Example
     public class ExampleSetup : MonoBehaviour
     {
         [SerializeField] private PoolingSlider _poolingSlider;
+        
+        //Your pooling slide prefab to use
         [SerializeField] private ExampleSlide _prefab;
         [Space] 
         [SerializeField] private Sprite _example1;
@@ -13,29 +15,29 @@ namespace PoolingSlider.Example
 
         private void Start()
         {
-            //List/Array with data
-            List<Example> _examples = new();
+            //fill up List/Array
+            List<Example> examples = new();
             for (int i = 0; i < 128; i++)
             {
-                _examples.Add(new Example(Color.red, _example1));
-                _examples.Add(new Example(Color.yellow, _example2));
-                _examples.Add(new Example(Color.green, _example1));
-                _examples.Add(new Example(Color.cyan, _example2));
-                _examples.Add(new Example(Color.blue, _example1));
-                _examples.Add(new Example(Color.magenta, _example2));
-                _examples.Add(new Example(Color.white, _example1));
-                _examples.Add(new Example(Color.red, _example2));
-                _examples.Add(new Example(Color.yellow, _example1));
-                _examples.Add(new Example(Color.green, _example2));
-                _examples.Add(new Example(Color.cyan, _example1));
-                _examples.Add(new Example(Color.blue, _example2));
-                _examples.Add(new Example(Color.magenta, _example1));
-                _examples.Add(new Example(Color.white, _example2));
+                examples.Add(new Example(Color.red, _example1));
+                examples.Add(new Example(Color.yellow, _example2));
+                examples.Add(new Example(Color.green, _example1));
+                examples.Add(new Example(Color.cyan, _example2));
+                examples.Add(new Example(Color.blue, _example1));
+                examples.Add(new Example(Color.magenta, _example2));
+                examples.Add(new Example(Color.white, _example1));
+                examples.Add(new Example(Color.red, _example2));
+                examples.Add(new Example(Color.yellow, _example1));
+                examples.Add(new Example(Color.green, _example2));
+                examples.Add(new Example(Color.cyan, _example1));
+                examples.Add(new Example(Color.blue, _example2));
+                examples.Add(new Example(Color.magenta, _example1));
+                examples.Add(new Example(Color.white, _example2));
             }
 
             _poolingSlider.Constructor(_prefab, _examples, clickedExample =>
             {
-                //Returns Example
+                //Action where you click on slide
             });
         }
     }
--- README.md
+++ README.md
@@ -1,4 +1,72 @@
 # Unity-Pooling Slider UI
 ![](https://github.com/StayOnSofa/Unity-PoolingSlider/blob/main/Example.gif?raw=true)
 ## Overview
-The Unity Pooling Slider System is a design pattern and implementation aimed at optimizing the performance and efficiency of UI sliders.
+The Unity Pooling Slider System introduces a design pattern and implementation focused on enhancing the performance and efficiency of UI sliders.
+### Setting up
+To get started, follow these steps:
+1. Create a Scroll Rect and populate the View and Content fields.
+2. Add the PoolingSlider class to the Scroll Rect.
+
+![](https://github.com/StayOnSofa/Unity-PoolingSlider/blob/main/Example.png?raw=true)
+Next, create a class model from which the UI sliders will retrieve information:
+```C#
+//Model layer
+public class Example
+{
+    public Color Color { private set; get; }
+    public Sprite Sprite { private set; get; }
+        
+    public Example(Color color, Sprite sprite)
+    {
+        Color = color;
+        Sprite = sprite;
+    }
+}
+```
+To create a class that fills up your UI slides, use PoolingSlide<YourModel>:
+```C#
+//View layer
+public class ExampleSlide : PoolingSlide<Example>
+{
+    [SerializeField] private Image _background;
+    [SerializeField] private Image _image;
+
+    public override void Constructor(Example value)
+    {
+        _background.color = value.Color;
+        _image.sprite = value.Sprite;
+    }
+}
+```
+
+Initialize pooling slider with your data in a setup class:
+```C#
+//View layer
+ public class ExampleSetup : MonoBehaviour
+ {
+    [SerializeField] private PoolingSlider _poolingSlider;
+        
+    // Your pooling slide prefab to use
+    [SerializeField] private ExampleSlide _prefab;
+    
+    [Space] 
+    [SerializeField] private Sprite _example1;
+    [SerializeField] private Sprite _example2;
+
+    private void Start()
+    {
+        // Fill up List/Array
+        List<Example> examples = new List<Example>();
+        for (int i = 0; i < 128; i++)
+        {
+            examples.Add(new Example(Color.red, _example1));
+            // ...
+        }
+
+        _poolingSlider.Constructor(_prefab, examples, clickedExample =>
+        {
+            // Action to perform when a slide is clicked
+        });
+    }
+}
+```
